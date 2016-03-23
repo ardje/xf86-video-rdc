54d52328759e64337a49dc62412159531484cd68
Ard van Breemen <ard@kwaak.net>
2016-03-23 11:54:38 +0000

    https://bugs.freedesktop.org/show_bug.cgi?id=57303

--- src/rdc_2dtool.c
+++ src/rdc_2dtool.c
@@ -35,7 +35,7 @@
 #include "xf86_OSproc.h"
 #include "xf86cmap.h"
 #include "compiler.h"
-#include "mibstore.h"
+//#include "mibstore.h"
 #include "vgaHW.h"
 #include "mipointer.h"
 #include "micmap.h"
